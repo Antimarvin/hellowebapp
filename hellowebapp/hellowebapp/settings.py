a3be18326a42e60909a5ebe0dd47eb4a978652a0
Ricky Sweat <antimarvin@gmail.com>
2015-09-04 18:25:07 -0400

    Commit with recipe typo fixed in recipeoptimizer

--- hellowebapp/hellowebapp/settings.py
+++ hellowebapp/hellowebapp/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    'recipieoptimizer',
+    'recipeoptimizer',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- hellowebapp/hellowebapp/urls.py
+++ hellowebapp/hellowebapp/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 
 urlpatterns = [
-	url(r'^$', 'recipieoptimizer.views.index', name='home'),
+	url(r'^$', 'recipeoptimizer.views.index', name='home'),
 	url(r'^about/$', 
 		TemplateView.as_view(template_name='about.html'),
 		name='about'),
@@ -11,10 +11,10 @@ urlpatterns = [
 		TemplateView.as_view(template_name='contact.html'),
 		name='contact'),
 	url(r'^recipes/(?P<slug>[-\w]+)/edit/$', 
-		'recipieoptimizer.views.recipe_detail',
+		'recipeoptimizer.views.recipe_detail',
 		name='recipe_detail'),
 	url(r'^recipes/(?P<slug>[-\w]+)/$', 
-		'recipieoptimizer.views.edit_recipe',
+		'recipeoptimizer.views.edit_recipe',
 		name='edit_recipe'),
     url(r'^admin/', include(admin.site.urls)),
 ]
