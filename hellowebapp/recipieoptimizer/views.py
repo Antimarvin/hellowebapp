ea34f650299b35dfe2d4b92df26afc4718330a3e
Antimarvin <Antimarvin@gmail.com>
2015-09-04 09:07:31 -0400

    Fun with django humanize app

--- hellowebapp/recipieoptimizer/views.py
+++ hellowebapp/recipieoptimizer/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+	number = 6
 	# this is your new view
-	return render(request, 'index.html')
+	return render(request, 'index.html', {
+		'number': number
+	})
